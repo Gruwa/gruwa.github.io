9b7a45eca0172e39606b22448e1472247f3010dc
Gruwa <alex.zolotarenko@gmail.com>
2016-12-12 02:06:05 +0200

    lesson_js1_20161212_v1

--- hw_js_0/js/script.js
+++ hw_js_0/js/script.js
@@ -346,3 +346,18 @@
 //     }
 //
 // }
+
+// function pow(x, n) {
+//   var result = 1;
+//   for(var i = 0; i < n; i++) {
+//       result *= x;
+//   }
+//   return result;
+// }
+// var x = prompt("x?", '');
+// var n = prompt("n?", '');
+// if (n < 0) {
+//   alert('Степень ' + n + 'не поддерживается, введите целую степень, большую 0');
+// } else {
+//   alert( pow(x, n) );
+// }
