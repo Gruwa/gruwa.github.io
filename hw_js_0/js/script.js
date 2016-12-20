f58d666e27754341bea445293e8a43784f51c129
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2016-12-20 18:22:30 +0200

    lesson_js1_20161220_v2

--- hw_js_0/js/script.js
+++ hw_js_0/js/script.js
@@ -527,3 +527,16 @@
 // user.surname = 'Петров';
 // user.name = 'Сергей';
 // delete user.name;
+
+// Определите, пуст ли объект
+
+function isEmpty(obj) {
+  /* ваш код */
+}
+
+var schedule = {};
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); // false
