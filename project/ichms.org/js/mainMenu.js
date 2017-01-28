2bf3bda56678ee281d58ee5eb3a7a9e5374775a1
Gruwa <alex.zolotarenko@gmail.com>
2017-01-29 01:53:19 +0200

    ichms.org_20170129_v1

---
+++ project/ichms.org/js/mainMenu.js
@@ -0,0 +1,17 @@
+
+    // Set options
+    var options = {
+        offset: '#showHere',
+        offsetSide: 'top',
+        classes: {
+            clone:   'banner--clone',
+            stick:   'banner--stick',
+            unstick: 'banner--unstick'
+        }
+    };
+
+    // Initialise with options
+    var banner = new Headhesive('.navigation', options);
+
+    // Headhesive destroy
+    // banner.destroy();
