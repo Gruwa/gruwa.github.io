b7dcbdf5e6be8c3a8caf0d45ec65b9208fc8bd6a
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-02-28 14:20:10 +0200

    hw_js_4_1_20170228_v2

--- practice/hw_js_4_1/js/script.js
+++ practice/hw_js_4_1/js/script.js
@@ -23,17 +23,17 @@ var app = {
 
             var div = {
                 tagName : 'div',
-                className : 'wrapper',
+                className : ['wrapper'],
             };
 
             var head = {
                 tagName : 'p',
-                className : ('header', 'page-header'),
+                className : ['header', 'page-header'],
                 content : ('Тест по программированию'),
             };
             var content = {
                 tagName : 'content',
-                className : 'content',
+                className : ['content'],
             };
             var button = {
                 tagName : 'button',
@@ -41,19 +41,19 @@ var app = {
             };
             var ol = {
                 tagName : 'ol',
-                className : ('list-group', 'testOl'),
+                className : ['list-group', 'testOl'],
             };
             var ul = {
                 tagName : 'ul',
-                className : ('list-group', 'clearfix:after'),
+                className : ['list-group', 'clearfix'],
             };
             var li = {
                 tagName : 'li',
-                className : 'list-group-item',
+                className : ['list-group-item'],
             };
-            var input = {
+            var INPUT = {
                 tagName : 'input',
-                className : 'input',
+                className : ['input'],
             };
 
 
@@ -91,12 +91,20 @@ var app = {
             //                 'tagName' : {'input' : {}},
             //                 'className' : {'input' : {}},
             //             },
+            // }
 
-                // }
 
 
-            var elem = new createElement(button);
-                        console.log(elem);
+            var box = new createElement(div);
+            document.body.insertBefore(box, box.nextSibling);
+            var elem = new createElement(head);
+            box.insertBefore(elem, elem.nextSibling);
+            var content = new createElement(content);
+            box.insertBefore(elem, elem.nextSibling);
+            elem = new createElement(button);
+            box.insertBefore(elem, elem.nextSibling);
+
+            console.log(elem);
 
             function createElement(config) {
               var element = document.createElement(config.tagName);
@@ -106,19 +114,58 @@ var app = {
                      element.classList.add(config.className[i]);
                   }
 
-              }
+              };
 
               if (config.content) {
                 element.innerHTML = config.content;
-              }
+            };
 
               if (config.tagName == 'button') {
                   var textButton = document.createTextNode('Проверить мои результаты');
                   element.appendChild(textButton);
-              }
+              };
 
               return element;
-            }
+                }
+
+            Test(data);
+            TestCheckBox(app);
+
+                function Test(obj) {
+                    for (var key in obj) {
+                        li = new createElement(li);
+                        li.innerHTML = key;
+                        ol = new createElement(ol);
+                        content.insertBefore(ol, ol.nextSibling);
+                        ol.appendChild(li);
+                        li.appendChild(TestLi(obj[key]));
+                        function TestLi(obj2) {
+                            ul = new createElement(ul);
+                            for (var key in obj2) {
+                                li =  new createElement(li);
+                                li.innerHTML = key;
+                                ul.appendChild(li);
+                            };
+                            return ul;
+                            }
+                        };
+
+                    }
+                function TestCheckBox(obj) {
+                        var allLi = document.querySelectorAll('ul > li');
+                        for (var i = 0; i < allLi.length; i++) {
+                            allLi[i].classList.add('listLi' + i);
+                            var input = new createElement(INPUT);
+                            allLi[i].insertAdjacentElement("beforeBegin", input);
+                        };
+                        var allInput = document.querySelectorAll('ul > input');
+                        for (var i = 0; i < allInput.length; i++) {
+                            var classNameInput = allInput[i].className = 'listInput' + i;
+                        }
+
+                }
+
+
 
 
         }
@@ -128,89 +175,83 @@ var app = {
 app.creatTest();
 
 
-
-//             DomElements(ListElement);
-//
-//             function DomElements(obj) {
-//                 for (var key in obj) {
-//
-//                     var element = document.createElement(key);
-//                     document.body.insertBefore(element, element.nextSibling);
-//
-//                     DomElementsList(obj[key], element);
-//
-//                     function DomElementsList(obj2, element) {
-//                         for (var key in obj2) {
-//                             element.classList.add(key) ;
-//                             if (key != 'wrapper') {
-//                                 var div = document.querySelector(".wrapper");
-//                                 div.insertBefore(element, element.nextSibling);
-//                                 };
-//                             if (key == 'testOl') {
-//                                 var div = document.querySelector(".content");
-//                                 div.insertBefore(element, element.nextSibling);
-//                                 };
-//                             if (key == 'header') {
-//                                 var div = document.querySelector(".header");
-//                                 div.innerHTML = 'Тест по программированию';
-//                                     }
-//                             if (key == 'button') {
-//                                 var div = document.querySelector(".button");
-//                                 var textButton = document.createTextNode('Проверить мои результаты');
-//                                 div.appendChild(textButton);
-//                             }
-//
-//                         };
-//                     }
-//                 };
-//
-//             }
-//
-//             Test(data);
-//             TestCheckBox(app);
-//
-//             function Test(obj) {
-//                 for (var key in obj) {
-//                     var li = document.createElement('li');
-//                     li.innerHTML = key;
-//                     var testOl = document.querySelector(".testOl");
-//                     testOl.appendChild(li);
-//                     li.appendChild(TestLi(obj[key]));
-//                     function TestLi(obj2) {
-//                         var ul = document.createElement('ul');
-//                         ul.classList.add('list-group', 'clearfix:after');
-//                         ul.style.marginTop = '10px';
-//                         ul.style.marginBottom = '10px';
-//                         for (var key in obj2) {
-//                             var li = document.createElement('li');
-//                             li.classList.add('list-group-item');
-//                             li.innerHTML = key;
-//                             ul.appendChild(li);
-//                         }
-//                         return ul;
-//                         }
-//                     }
-//
-//                 }
-//             function TestCheckBox(obj) {
-//                     var allLi = document.querySelectorAll('ul > li');
-//                     for (var i = 0; i < allLi.length; i++) {
-//                         allLi[i].classList.add('listLi' + i);
-//                         var input = document.createElement("INPUT");
-//                         input.setAttribute("type", "checkbox");
-//                         input.style.float = 'left';
-//                         allLi[i].insertAdjacentElement("beforeBegin", input);
-//                         input.style.marginRight = '5px';
-//                     }
-//                     var allInput = document.querySelectorAll('ul > input');
-//                     for (var i = 0; i < allInput.length; i++) {
-//                         var classNameInput = allInput[i].className = 'listInput' + i;
-//                     }
-//
-//             }
-//
-//         },
-//
-// }
-//
-// app.creatTest();
+            //
+            // DomElements(data);
+            //
+            // function DomElements(obj) {
+            //     for (var key in obj) {
+            //
+            //         var element = document.createElement(key);
+            //         document.body.insertBefore(element, element.nextSibling);
+            //
+            //         DomElementsList(obj[key], element);
+            //
+            //         function DomElementsList(obj2, element) {
+            //             for (var key in obj2) {
+            //                 element.classList.add(key) ;
+            //                 if (key != 'wrapper') {
+            //                     var div = document.querySelector(".wrapper");
+            //                     div.insertBefore(element, element.nextSibling);
+            //                     };
+            //                 if (key == 'testOl') {
+            //                     var div = document.querySelector(".content");
+            //                     div.insertBefore(element, element.nextSibling);
+            //                     };
+            //                 if (key == 'header') {
+            //                     var div = document.querySelector(".header");
+            //                     div.innerHTML = 'Тест по программированию';
+            //                         }
+            //                 if (key == 'button') {
+            //                     var div = document.querySelector(".button");
+            //                     var textButton = document.createTextNode('Проверить мои результаты');
+            //                     div.appendChild(textButton);
+            //                 }
+            //
+            //             };
+            //         }
+            //     };
+            //
+            // }
+            //
+            // Test(data);
+            // TestCheckBox(app);
+            //
+            // function Test(obj) {
+            //     for (var key in obj) {
+            //         var li = document.createElement('li');
+            //         li.innerHTML = key;
+            //         var testOl = document.querySelector(".testOl");
+            //         testOl.appendChild(li);
+            //         li.appendChild(TestLi(obj[key]));
+            //         function TestLi(obj2) {
+            //             var ul = document.createElement('ul');
+            //             ul.classList.add('list-group', 'clearfix:after');
+            //             ul.style.marginTop = '10px';
+            //             ul.style.marginBottom = '10px';
+            //             for (var key in obj2) {
+            //                 var li = document.createElement('li');
+            //                 li.classList.add('list-group-item');
+            //                 li.innerHTML = key;
+            //                 ul.appendChild(li);
+            //             }
+            //             return ul;
+            //             }
+            //         }
+            //
+            //     }
+            // function TestCheckBox(obj) {
+            //         var allLi = document.querySelectorAll('ul > li');
+            //         for (var i = 0; i < allLi.length; i++) {
+            //             allLi[i].classList.add('listLi' + i);
+            //             var input = document.createElement("INPUT");
+            //             input.setAttribute("type", "checkbox");
+            //             input.style.float = 'left';
+            //             allLi[i].insertAdjacentElement("beforeBegin", input);
+            //             input.style.marginRight = '5px';
+            //         }
+            //         var allInput = document.querySelectorAll('ul > input');
+            //         for (var i = 0; i < allInput.length; i++) {
+            //             var classNameInput = allInput[i].className = 'listInput' + i;
+            //         }
+            //
+            // }
