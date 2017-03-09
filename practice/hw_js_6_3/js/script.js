e227bd4274f648ff12d7c8d78cc92418c55d3c00
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-03-09 16:05:35 +0200

    hw_js_6_3_20170309_v1

--- practice/hw_js_6_3/js/script.js
+++ practice/hw_js_6_3/js/script.js
@@ -34,75 +34,79 @@ function Stopwatch(elem) {
         var time = 0;
         var interval;
         var offset;
+
         this.isOn = false;
         this.start = function() {
-                if (!this.isOn) {
-                    interval = setInterval(this.update.bind(this), 10);
-                    offset = Date.now();
-                    this.isOn = true;
-                };
+            if (!this.isOn) {
+                interval = setInterval(this.update.bind(this), 10);
+                offset = Date.now();
+                this.isOn = true;
             };
+        };
         this.stop = function () {
-                if (this.isOn) {
-                    clearInterval(interval);
-                    interval = null;
-                    this.isOn = false;
-                };
-            };
-        this.reset = function () {
+            if (this.isOn) {
                 clearInterval(interval);
                 interval = null;
                 this.isOn = false;
-                time = 0;
-                this.update();
             };
+        };
+        this.reset = function () {
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+            time = 0;
+            this.update();
+        };
         this.split = function() {
             this.result();
         };
         this.update = function() {
-                if (this.isOn) {
-                    time += delta();
-                };
-                var formattedTime = timeFormatter(time);
-                elem.textContent = formattedTime;
-            }
+            if (this.isOn) {
+                time += delta();
+            };
+
+            var formattedTime = timeFormatter(time);
+
+            elem.textContent = formattedTime;
+        }
         this.result = function() {
             var element = document.createElement('p');
             var box = document.getElementsByClassName('content');
+
             if (!watch.isOn) {
                 element.innerHTML = ++number + '. ' + 'Stop ' + timer.innerHTML;
             } if (watch.isOn) {
                 element.innerHTML = ++number + '. ' + 'Split ' + timer.innerHTML;
             } else {};
+
             element.classList.add('pList');
             box[0].insertBefore(element, element.nextSibling);
         }
         function delta() {
-                var now = Date.now();
-                var timePassed = now - offset;
-                offset = now;
-                return timePassed;
-            }
+            var now = Date.now();
+            var timePassed = now - offset;
+            offset = now;
+            return timePassed;
+        }
         function timeFormatter(timeInMilliseconds) {
-                var time = new Date(timeInMilliseconds);
-                var hours = '0';
-                var minutes = time.getMinutes().toString();
-                var seconds = time.getSeconds().toString();
-                var milliseconds = time.getMilliseconds().toString();
+            var time = new Date(timeInMilliseconds);
+            var hours = '0';
+            var minutes = time.getMinutes().toString();
+            var seconds = time.getSeconds().toString();
+            var milliseconds = time.getMilliseconds().toString();
 
-                if (hours.length < 2) {
-                    hours = '0' + hours;
-                }
-                if (minutes.length < 2) {
-                    minutes = '0' + minutes;
-                }
-                if (seconds.length < 2) {
-                    seconds = '0' + seconds;
-                }
-                while (milliseconds.length < 3) {
-                    milliseconds = '0' + milliseconds;
-                }
-                return hours + ' : ' + minutes + ' : ' + seconds + ' : ' + milliseconds
+            if (hours.length < 2) {
+                hours = '0' + hours;
             }
-
-    }
+            if (minutes.length < 2) {
+                minutes = '0' + minutes;
+            }
+            if (seconds.length < 2) {
+                seconds = '0' + seconds;
+            }
+            while (milliseconds.length < 3) {
+                milliseconds = '0' + milliseconds;
+            }
+            return hours + ' : ' + minutes + ' : ' + seconds + ' : ' + milliseconds;
+        }
+}
