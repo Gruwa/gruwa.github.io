1cc0857b42b0aced9b9fa95aefcf14b7a580e3e6
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-03-13 04:40:04 +0200

    hw_js_8_20170313_v3

--- practice/hw_js_9/js/script.js
+++ practice/hw_js_9/js/script.js
@@ -1,20 +1,42 @@
 'use strict';
 
 $(document).ready(function() {
-    var allPhoto = 10;
-    var html_elem = '', k = "<li><img class='jcarousel_img'></li>";
-    
-    for (var i = 1; i <= allPhoto; i++) {
-        $('.jcarousel').find('ul').html(k).find('img:last').attr({src: './img/'+i+'.jpg', width: '640', height: '480'});
-        var last = $('ul:last').html();
-        html_elem += last;
-    };
-    $('.jcarousel').find('ul').html(html_elem);
+    // start jcarousel
+    $(function() {
+        var allPhoto = 10;
 
+        photoGallary(allPhoto);
 
-    $('.jcarousel').jcarousel({
+        function photoGallary(key) {
+            var html_elem = '', k = "<li><img class='jcarousel_img'></li>";
+
+            for (var i = 1; i <= key; i++) {
+                $('.jcarousel').find('ul').html(k).find('img:last').attr({src: './img/'+i+'.jpg', width: '640', height: '480'});
+                var last = $('ul:last').html();
+                html_elem += last;
+            };
+            $('.jcarousel').find('ul').html(html_elem);
+        }
+
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel')
+            .jcarousel('reload', {
+                animation: 400
+            })
+            .jcarousel({
+                wrap: 'both'
+            });
+
+        $('.jcarousel-prev').click(function() {
+            $('.jcarousel').jcarousel('scroll', '-=1');
+        });
+
+        $('.jcarousel-next').click(function() {
+            $('.jcarousel').jcarousel('scroll', '+=1');
+        });
 
     });
+    // end jcarousel
+
 });
-// .add('img').add({src: "'img/'+ ++i +'.jpg'", width: '600px', height: '400'})
-// <img src="../_shared/img/img1.jpg" width="600" height="400" alt="">
