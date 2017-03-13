6d5485d9c3af1c95842c4a2f6eab71d9825c961d
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-03-13 22:11:21 +0200

    hw_js_8_20170313_v4

--- practice/hw_js_9/js/script.js
+++ practice/hw_js_9/js/script.js
@@ -2,41 +2,47 @@
 
 $(document).ready(function() {
     // start jcarousel
-    $(function() {
-        var allPhoto = 10;
+        $(function() {
+            var $allPhoto = 10;
 
-        photoGallary(allPhoto);
+            photoGallary($allPhoto);
 
-        function photoGallary(key) {
-            var html_elem = '', k = "<li><img class='jcarousel_img'></li>";
+            function photoGallary(key) {
+                var $html_elem = '', $k = "<li><img class='jcarousel_img'></li>";
 
-            for (var i = 1; i <= key; i++) {
-                $('.jcarousel').find('ul').html(k).find('img:last').attr({src: './img/'+i+'.jpg', width: '640', height: '480'});
-                var last = $('ul:last').html();
-                html_elem += last;
-            };
-            $('.jcarousel').find('ul').html(html_elem);
-        }
+                for (var i = 1; i <= key; i++) {
+                    $('.jcarousel').find('ul').html($k).find('img:last').attr({src: './img/'+i+'.jpg', width: '640', height: '480'});
+                    var $last = $('ul:last').html();
+                    $html_elem += $last;
+                };
+                $('.jcarousel').find('ul').html($html_elem);
+            }
 
-        $('.jcarousel').jcarousel();
+            $('.jcarousel').jcarousel();
 
-        $('.jcarousel')
-            .jcarousel('reload', {
-                animation: 400
-            })
-            .jcarousel({
-                wrap: 'both'
+            $('.jcarousel')
+                .jcarousel('reload', {
+                    animation: 400
+                })
+                .jcarousel({
+                    wrap: 'both'
+                });
+
+            $('.jcarousel-prev').click(function() {
+                $('.jcarousel').jcarousel('scroll', '-=1');
             });
 
-        $('.jcarousel-prev').click(function() {
-            $('.jcarousel').jcarousel('scroll', '-=1');
-        });
+            $('.jcarousel-next').click(function() {
+                $('.jcarousel').jcarousel('scroll', '+=1');
+            });
 
-        $('.jcarousel-next').click(function() {
-            $('.jcarousel').jcarousel('scroll', '+=1');
         });
-
-    });
     // end jcarousel
+
+    // start ikSelect
+        $(function functionName() {
+            $('select').ikSelect();
+        })
+    // end ikSelect
 
 });
