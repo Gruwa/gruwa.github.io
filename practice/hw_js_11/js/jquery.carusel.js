5295664a31df61f23008913f9cbff0d194b71b14
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-03-20 21:33:58 +0200

    hw_js_11_20170320_v3

--- practice/hw_js_11/js/jquery.carusel.js
+++ practice/hw_js_11/js/jquery.carusel.js
@@ -36,6 +36,7 @@
                 'border-radius' : '5px',
                 'padding' : '5px',
                 'margin-top' : '25px',
+                'cursor': 'pointer',
             });
             $('.carousel-arrow-left').css({
                 'margin-right' : '25px',
@@ -65,10 +66,21 @@
         var elementsList = $('.carousel-list');
         var pixelsOffset = 125;
         var currentLeftValue = 0;
+        var elementsCount = elementsList.find('li').length;
+        var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
+        var maximumOffset = 0;
 
         leftUIEl.click(function() {
+            if (currentLeftValue != maximumOffset) {
+                currentLeftValue += 125;
+                elementsList.animate({ left : currentLeftValue + "px"}, 500);
+            };
         });
        rightUIEl.click(function() {
+           if (currentLeftValue != minimumOffset) {
+                currentLeftValue -= 125;
+                elementsList.animate({ left : currentLeftValue + "px"}, 500);
+            };
         });
 
 
