3ce310eaa1f322847224f18924e0ba1dc901a590
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-03-21 03:07:41 +0200

    hw_js_11_20170321_v1

--- practice/hw_js_11/js/script.js
+++ practice/hw_js_11/js/script.js
@@ -5,5 +5,26 @@
         $quantityImg: '10',
         $widthImg : '100'
     });
+    var $html = $('#homework1').html();
+    var $data = {
+        name : 'Золотаренко Алексей Дмитриевич',
+        img : 'wooo.png',
+        text : 'Мы все учились',
+        title2 : 'Хочу учить фронтенд, потому что:',
+        title3 : 'Мой контактный телефон:',
+        content_4 : '+38022222222222',
+        title4 : 'Мой профиль Вконтакте:',
+        content_5_link : 'https://vk.com/',
+        content_5_name : 'vk.com',
+        title5 : 'Мой фидбек:',
+        content_6 : 'Если нужно, могу построить вам нечто',
+        menu_list1 : 'Заборы строить не интересно',
+        menu_list2 : 'Платят мало',
+        menu_list3 :  'Приходится работать по ночам',
+    };
+    var $content = tmpl($html, $data);
+    $('.wrapper').append($content);
+
+
 
 })(jQuery);
