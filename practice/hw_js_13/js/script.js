80cae1d0a823da0a2a85f17eec86190ca9793dd2
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-03-22 23:00:14 +0200

    hw_js_11_20170322_v1

---
+++ practice/hw_js_13/js/script.js
@@ -0,0 +1,10 @@
+// var leader = {
+//   name: "Василий Иванович",
+//   age: 35
+// };
+//
+// var str = JSON.stringify(leader);
+// console.log(str);
+//
+// leader = JSON.parse(str);
+// console.log(leader);
