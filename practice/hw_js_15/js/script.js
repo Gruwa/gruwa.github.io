bcbb09f7ed15916e67468bba8ce78c596e9ac27c
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-11 13:15:46 +0300

    hw_js_15_20170411_v1

--- practice/hw_js_15/js/script.js
+++ practice/hw_js_15/js/script.js
@@ -1,26 +1,31 @@
 $(function() {
 
-    $('#submitTenor').on('click', function(event) {
-        event.preventDefault();
+    $('form').submit(function(event) {
         /* Act on the event */
-        var $value = $('#searchTenor').value; 
+        var $value = $('#searchTenor').val();
+
 
         $.ajax({
-            url: 'https://api.tenor.co/v1/gifs?key=LIVDSRZULELA&ids='+,
-            type: 'default GET (Other values: POST)',
-            dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-            data: {param1: 'value1'}
-        })
-        .done(function() {
+            url: 'https://pixabay.com/api/',
+            method: 'GET',
+            // dataType: 'jsonp',
+            data: {
+                key: '5069177-dbb9700ef5f333933be917c5e',
+                q: $value,
+                image_type: 'photo',
+
+
+
+            }
+        }).done(function() {
             console.log("success");
-        })
-        .fail(function() {
+        }).fail(function() {
             console.log("error");
-        })
-        .always(function() {
+        }).always(function() {
+            console.log($value);
+            // console.log($.ajax);
             console.log("complete");
         });
-
 
     });
 
