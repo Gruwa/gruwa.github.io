3123d1d7a827b5f1389e497df447a97226f29a15
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-15 03:40:38 +0300

    hw_js_18_20170415_v2

--- practice/hw_js_18/gulpfile.js
+++ practice/hw_js_18/gulpfile.js
@@ -1,17 +1,27 @@
 'use strict';
 
 var gulp = require ('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var concat = require ('gulp-concat');
+var uglify = require ('gulp-uglify');
+var debug = require ('gulp-debug')
 
 gulp.task('default', function () {
     gulp.src('css/src/**/*.css')
+    .pipe(debug({title: 'src'}))
     // .pipe(uglify())
+    // .pipe(debug({title: 'uglify'}))
     .pipe(concat('style.main.min.css'))
-    .pipe(gulp.dest('css/dest'));
+    .pipe(debug({title: 'concat'}))
+    .pipe(gulp.dest('css/dest'))
+    .pipe(debug({title: 'dest'}));
     gulp.src('js/src/**/*.js')
+    .pipe(debug({title: 'src'}))
     // .pipe(uglify())
+    // .pipe(debug({title: 'uglify'}))
     .pipe(concat('script.main.min.js'))
-    .pipe(gulp.dest('js/dest'));
+    .pipe(debug({title: 'concat'}))
+    .pipe(gulp.dest('js/dest'))
+    .pipe(debug({title: 'dest'}));
+    
     return;
 });
