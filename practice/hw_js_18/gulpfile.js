7d4d030e318b9fef3732edbfadc084f62750fcb2
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-15 03:02:16 +0300

    hw_js_18_20170415_v1

--- practice/hw_js_18/gulpfile.js
+++ practice/hw_js_18/gulpfile.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const gulp = require ('gulp');
+var gulp = require ('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
-    return gulp.src(['css/src/*.css','js/src/*.js'])
-    .pipe(gulp.dest(function(file){
-        return file.extname == '.js' ? 'js/dest' : 'css/dest';
-    }));
+    gulp.src('css/src/**/*.css')
+    // .pipe(uglify())
+    .pipe(concat('style.main.min.css'))
+    .pipe(gulp.dest('css/dest'));
+    gulp.src('js/src/**/*.js')
+    // .pipe(uglify())
+    .pipe(concat('script.main.min.js'))
+    .pipe(gulp.dest('js/dest'));
+    return;
 });
