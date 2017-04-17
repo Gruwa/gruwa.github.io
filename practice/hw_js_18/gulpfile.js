dc86abf9335b0c5878b80e9a7c5a3c36466143e8
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-17 15:34:27 +0300

    hw_js_18_20170417_v1

--- practice/hw_js_18/gulpfile.js
+++ practice/hw_js_18/gulpfile.js
@@ -9,12 +9,15 @@ var stylus = require ('gulp-stylus'); //хороший вопрос
 var uncss = require('gulp-uncss'); //удаляет не используемый цсс
 var uglifycss = require('gulp-uglifycss'); //минимизация файлов css
 
-gulp.task('default', function () {
+
+gulp.task('default', function (callback) {
     gulp.src('css/src/**/*.css')
-        // .pipe(stylus())
-        .pipe(sourcemaps.init())
         .pipe(debug({title: 'src'}))
+        // .pipe(stylus())
+        .pipe(debug({title: 'stylus'}))
+        .pipe(sourcemaps.init())
         .pipe(uncss({html: 'index.html'}))
+        .pipe(debug({title: 'uncss'}))
         .pipe(uglifycss())
         .pipe(debug({title: 'uglify'}))
         .pipe(concat('style.main.min.css'))
@@ -34,7 +37,8 @@ gulp.task('default', function () {
         .pipe(gulp.dest('js/dest'))
         .pipe(debug({title: 'dest'}));
 
-    return;
+    callback();
 });
-// gulp.watch('js/src/**/*.*', gulp.series('default'));
+
+// gulp.watch('js/src/**/*.*', gulp.parallel('default'));
 // gulp.watch('css/src/**/*.*', gulp.series('default'));
