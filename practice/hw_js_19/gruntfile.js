93cb9fdebd940abb1ad7a96f29eb69908c0a55f4
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-18 04:45:20 +0300

    hw_js_19_20170418_v1

--- practice/hw_js_19/gruntfile.js
+++ practice/hw_js_19/gruntfile.js
@@ -4,23 +4,58 @@ module.exports = function(grunt) {
               dist: {
                   files: [{
                       expand: true,
-                      cwd: 'style/src',
+                      cwd: 'css/src',
                       src: ['style.scss'],
-                      dest: 'style/dest',
+                      dest: 'css/dest',
                       ext: '.css'
                   }]
               }
           },
           cssmin: {
               dest: {
-                src: ['style/dest/style.css'],
-                dest: 'style/dest/style.min.css'
+                src: ['css/dest/style.css'],
+                dest: 'css/dest/style.min.css'
               }
-          }
+          },
+          concat: {
+              dest: {
+                src: ['js/src/*.js'],
+                dest: 'css/dest/script.js'
+              }
+          },
+          uglify: {
+              dest: {
+                src: ['js/dest/script.js'],
+                dest: 'js/dest/script.min.js'
+              }
+          },
+          watch: {
+            sass: {
+                // We watch and compile sass files as normal but don't live reload here
+                files: ['css/src/*.scss'],
+                tasks: ['sass']
+            },
+            cssmin: {
+                // We watch and compile sass files as normal but don't live reload here
+                files: ['css/dest/style.css'],
+                tasks: ['cssmin']
+            },
+            concat: {
+                 // We watch and compile sass files as normal but don't live reload here
+                 files: ['js/src/*.js'],
+                 tasks: ['concat']
+            },
+            uglify: {
+                // We watch and compile sass files as normal but don't live reload here
+                files: ['js/dest/script.js'],
+                tasks: ['uglify']
+            }
+         }
       });
+      grunt.loadNpmTasks('grunt-contrib-concat');
       grunt.loadNpmTasks('grunt-contrib-uglify');
       grunt.loadNpmTasks('grunt-contrib-cssmin');
-      grunt.loadNpmTasks('grunt-concat-css');
       grunt.loadNpmTasks('grunt-contrib-sass');
-      grunt.registerTask('default', ['sass','cssmin']);
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.registerTask('default', ['sass','cssmin','concat','uglify','watch']);
 };
