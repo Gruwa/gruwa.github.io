f3352633f2c7908a09df0088da2395c199ff87e3
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-20 19:57:40 +0300

    hw_js_19_20170420_v2

--- practice/hw_js_19/js/dest/script.js
+++ practice/hw_js_19/js/dest/script.js
@@ -13,5 +13,26 @@ $(function () {
 
 $(function () {
     'use strict';
+    console.log($('#___gcse_0'));
+    $('#gsc-i-id1').on('click', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        this.css('background-color', 'rgba(53, 56, 60, 5)');
+    });
 
 });
+
+// .Rectangle_2_copy_2 {
+//   border-style: solid;
+//   border-width: 1px;
+//   border-color: rgb(24, 26, 29);
+//   background-image: -moz-linear-gradient( 90deg, rgb(0,1,1) 0%, rgb(255,255,255) 100%);
+//   background-image: -webkit-linear-gradient( 90deg, rgb(0,1,1) 0%, rgb(255,255,255) 100%);
+//   background-image: -ms-linear-gradient( 90deg, rgb(0,1,1) 0%, rgb(255,255,255) 100%);
+//   position: absolute;
+//   left: 1289px;
+//   top: 116px;
+//   width: 243px;
+//   height: 42px;
+//   z-index: 16;
+// }
