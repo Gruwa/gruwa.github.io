7f6c366380b3e96421a1a375af3cd03ec9799f44
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-25 06:10:04 +0300

    hw_js_19_20170425_v1

--- practice/hw_js_19/js/dest/script.js
+++ practice/hw_js_19/js/dest/script.js
@@ -1660,6 +1660,23 @@ $(function () {
     var $content = tmpl($html, $dataTmpl);
     $('#latestNewsIn').html('');
     $('#latestNewsIn').append($content);
+//news-box
+    $('.panel').on('click', function(e) {
+        var elem = $(e.target).parent();
+        console.log(elem);
+        console.log($('.panel').find('p.accordion-panel-focus'));
+        if ($('.panel').find('p.accordion-panel-focus').length != 0) {
+            $('.panel').find('.accordion-panel').removeClass('accordion-panel-focus');
+            $('.panel').find('.accordion-plus').removeClass('accordion-plus-focus');
+            $('.panel').removeClass('panel-focus');
+        } else {
+            $(e.target).find('.accordion-panel').addClass('accordion-panel-focus');
+            $(e.target).find('.accordion-plus').addClass('accordion-plus-focus');
+            $(e.target).addClass('panel-focus');
+        }
+
+        /* Act on the event */
+    });
 });
 
 // see:
