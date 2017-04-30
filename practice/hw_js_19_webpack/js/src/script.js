995169ee82a50f247c432cad21c0992d52327625
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-04-30 13:25:57 +0300

    hw_js_19_webpack_20170430_v2

--- practice/hw_js_19_webpack/js/src/script.js
+++ practice/hw_js_19_webpack/js/src/script.js
@@ -1,8 +1,8 @@
 'use strict';
 // Подключение всех нужный скрипт файлов
+var welcome3 = require('./template'); // в формате старом
 import welcome from './myscript'; // в формате ES2015
-// var welcome3 = require('./template'); // в формате старом
 
 // для доступа ко всем переменным в файлах
 exports.welcome = welcome;
-// exports.welcome3 = welcome3;
+exports.welcome3 = welcome3;
