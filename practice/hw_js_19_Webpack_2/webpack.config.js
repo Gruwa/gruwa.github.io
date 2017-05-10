551b86c7c839b4495f3033ed87b8aed2358a3070
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-05-10 05:10:26 +0300

    hw_js_19_webpack2_20170510_v3

---
+++ practice/hw_js_19_Webpack_2/webpack.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+    module.exports = {
+        entry: "./project/js/script",
+            output: {
+                path: __dirname + "/build/js",
+                filename: "build.js"
+            }
+    };
