69605c3ab97b15f4166a4d1f506ac41d4cf7ce6a
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-05-10 05:05:25 +0300

    hw_js_19_webpack2_20170510_v1

---
+++ practice/hw_js_19_webpack_2/project/js/google.js
@@ -0,0 +1,14 @@
+module.exports = function google() {
+    $(function () {
+        'use strict';
+      (function() {
+        var cx = '012577494148929298528:dq_ytgguf-c';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+      })();
+    });
+};
---
+++ practice/hw_js_19_webpack_2/project/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const google = require('./google');
+const bxslider = require('./bxslider');
+const template = require('./template');
+const myscript = require('./myscript');
+
+google();
+bxslider();
+template();
+myscript();
