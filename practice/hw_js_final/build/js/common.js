9ca9b449cc3fc50943db5c72387b32ed87ac978c
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-06-24 05:23:31 +0300

    hw_js_final_20170624_7

--- practice/hw_js_final/build/js/common.js
+++ practice/hw_js_final/build/js/common.js
@@ -88,7 +88,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "2e782041658d5163bbd5"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "aea3ed7bafaefbb20007"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
