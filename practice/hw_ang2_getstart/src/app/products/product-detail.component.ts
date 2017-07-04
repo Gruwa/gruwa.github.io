46aa910d4594ce08e7a01a167d4bcb5402a995cf
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-07-04 05:03:14 +0300

    hw_ang_20170704_v1

--- practice/hw_ang2_getstart/src/app/products/product-detail.component.ts
+++ practice/hw_ang2_getstart/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -6,7 +7,15 @@ import { IProduct } from './product';
     templateUrl: 'app/products/product-detail.component.html'
 })
 
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
 }
