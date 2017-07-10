67178d3df788d2da1ce3ee3f4b9946a56a345564
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-07-10 19:25:26 +0300

    hw_ang2_20170710_v1

--- practice/hw_ang2_fund/app/events/event-thumbnail.component.ts
+++ practice/hw_ang2_fund/app/events/event-thumbnail.component.ts
@@ -10,17 +10,26 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     <div>Price: \$ {{event.price}}</div>
     <div>
         <span>Lcation: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
     </div>
-    <!--<button class="btn btn-primary" (click)="handleClickMe()">Click Me</button>-->
+    <!--
+    <button class="btn btn-primary" (click)="handleClickMe()">Click Me</button>
+    По клику на кнопку в дочернем эелементе вызывается функция handleClickMe(),
+    которая вытягивает свойство this.event.name и с помощью @Output() eventClick = new EventEmitter();
+    передает его родительнскому элементу
+    -->
 </div>
-    `
+    `,
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: red; }
+    `]
 })
 export class EventThumbnailComponent {
     @Input() event:any;
+    someProperty:any = "some value";
 
-    // @Output() eventClick = new EventEmitter();
+// @Output() eventClick = new EventEmitter();
 
 //    handleClickMe() {
 //        this.eventClick.emit(this.event.name);
