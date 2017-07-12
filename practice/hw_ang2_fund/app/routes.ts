aed9342acf4a1da0e0c827c2c91e364cc9ed0e10
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-07-12 06:17:37 +0300

    hw_ang2_20170712_v3

--- practice/hw_ang2_fund/app/routes.ts
+++ practice/hw_ang2_fund/app/routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 
+import { CreateEventComponent } from './events/creat-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventsListComponent } from './events/events-list.component';
 
 export const appRoutes:Routes = [
+    { path: 'events/new', component: CreateEventComponent },
     { path: 'events', component: EventsListComponent },
     { path: 'events/:id', component: EventDetailsComponent },
     { path: '', redirectTo: '/events', pathMatch: 'full' } // этой строчкой выполняется
     //редирект, если будет пустой путь то происходит перенаправление на путь 
     // '/events' 
-]
+] 
