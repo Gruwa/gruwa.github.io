1690ef8e46e25aee3912300ec1227e776a09bdf1
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-07-27 01:16:00 +0300

    add_hw_ang2_wp_wunderlist

---
+++ practice/hw_ang2_wp_wunderlist/config/webpack.common.img.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const webpack   = require('webpack');
+const path      = require('path');
+
+module.exports = {
+    config: {
+        test: /\.(png|jpe?g|gif|ico|svg)$/,
+        use: [
+        {
+            loader: 'file-loader',
+            options: {
+            name: 'assets/img/[name].[ext]?[hash:6]'
+            }
+        },
+        {
+            loader: 'image-webpack-loader',
+            options: {
+            mozjpeg: {
+                quality: 65
+            },
+            pngquant:{
+                quality: "50-70",
+                speed: 4
+            },
+            svgo:{
+                plugins: [
+                {
+                    removeViewBox: false
+                },
+                {
+                    removeEmptyAttrs: false
+                }
+                ]
+            },
+            gifsicle: {
+                optimizationLevel: 6,
+                interlaced: false
+            },
+            optipng: {
+                optimizationLevel: 6,
+                interlaced: false
+            }
+            }
+          }
+        ]
+    }
+};
