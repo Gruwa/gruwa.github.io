ea321ae7e9d7285a52f93c319e6878b8efc4b5de
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-08-07 20:57:07 +0300

    hw_ts_20170807_v2

--- practice/hw_ts/car.js
+++ practice/hw_ts/car.js
@@ -1,7 +1,18 @@
 "use strict";
 var Car = (function () {
     function Car(engine) {
-        this.engineType = engine;
+        this.engine = engine;
     }
+    Car.prototype.start = function () {
+        alert('Engine started' + this.engine);
+    };
+    Car.prototype.stop = function () {
+        alert('Engine stopped' + this.engine);
+    };
     return Car;
 }());
+window.onload = function () {
+    var car = new Car('V12');
+    car.start();
+    car.stop();
+};
--- practice/hw_ts/car.ts
+++ practice/hw_ts/car.ts
@@ -1,6 +1,21 @@
 class Car {
-    engineType: number;
-    constructor(engine: number) {
-        this.engineType = engine;
+    engine: string;
+
+    constructor( engine: string) {
+        this.engine = engine;
     }
+
+    start() {
+        alert('Engine started' + this.engine);
+    }
+
+    stop() {
+        alert('Engine stopped' + this.engine);
+    }
+}
+
+window.onload = function() {
+    let car = new Car('V12');
+    car.start();
+    car.stop();
 }
