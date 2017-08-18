4d2d0a5123eea6515bca08878e31683a5237cf94
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-08-19 01:55:29 +0300

    hw_ang2wp_wund_20170819_v2

--- practice/hw_ang2_wp_wunderlist_v2/src/app/content/create-content.component.ts
+++ practice/hw_ang2_wp_wunderlist_v2/src/app/content/create-content.component.ts
@@ -4,8 +4,16 @@ import { ActivatedRoute } from '@angular/router';
 import { CreateSidebarComponent } from '../sidebar/index';
 
 @Component({
+    providers: [CreateSidebarComponent],
     templateUrl: './create-content.component.html'
 })
+
 export class CreateContentComponent {
 
+    constructor(private createSidebarComponent: CreateSidebarComponent) {
+
+    }
+    creatNewLists() {
+        this.createSidebarComponent.creatNewLists();
+    }
 }
