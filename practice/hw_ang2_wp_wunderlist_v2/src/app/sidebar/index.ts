814d80c68342818f86fe5c2d291b8e81ee5ae39d
Aleksey D. Zolotarenko <alex.zolotarenko@gmail.com>
2017-08-31 23:01:36 +0300

    hw_ang2wp_wunder_20170831_v1

--- practice/hw_ang2_wp_wunderlist_v2/src/app/sidebar/index.ts
+++ practice/hw_ang2_wp_wunderlist_v2/src/app/sidebar/index.ts
@@ -3,6 +3,5 @@ export * from './events/events-list.component';
 export * from './red-pencil/red-pencil.component';
 export * from './new-events/new-events.component';
 export * from './events/show-pencil-events-list.directive';
-export * from './events/pencil-events-list.directive';
 export * from './search/search-bar.component';
 export * from './search/result.component';
