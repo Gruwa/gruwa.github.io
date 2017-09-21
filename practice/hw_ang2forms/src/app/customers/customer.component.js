ea7182fb2b35ed0990cb542731efaaaed00619ab
Gruwa <alex.zolotarenko@gmail.com>
2017-09-21 11:54:25 +0300

    hw_ang2forms_20170921

---
+++ practice/hw_ang2forms/src/app/customers/customer.component.js
@@ -0,0 +1,27 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var customer_1 = require("./customer");
+var CustomerComponent = (function () {
+    function CustomerComponent() {
+        this.customer = new customer_1.Customer();
+    }
+    CustomerComponent.prototype.save = function (customerForm) {
+        console.log(customerForm.form);
+        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    };
+    return CustomerComponent;
+}());
+CustomerComponent = __decorate([
+    core_1.Component({
+        selector: 'my-signup',
+        templateUrl: './app/customers/customer.component.html'
+    })
+], CustomerComponent);
+exports.CustomerComponent = CustomerComponent;
+//# sourceMappingURL=customer.component.js.map
