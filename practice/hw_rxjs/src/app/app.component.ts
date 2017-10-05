425f0257cc82cc16573da7d48c719be230687706
Gruwa <alex.zolotarenko@gmail.com>
2017-10-05 22:09:29 +0300

    hw_rxjs_20171005_v2

--- practice/hw_rxjs/src/app/app.component.ts
+++ practice/hw_rxjs/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from "rxjs/Observable";
+// import 'rxjs/add/operator/map'; если потребует установить функции
 
 import '../assets/style/style.scss';
 
@@ -10,4 +13,75 @@ declare let require: (filename: string) => any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+  
+  
+    ngOnInit() {
+        let numbers = [1,5,10];
+         // let source = Observable.from(numbers); // 1 способ задание потока
+
+        // 2 способ задания потока-------------------
+        // let source = Observable.create(observer => {
+            
+        //     for(let n of numbers) {
+
+        //         // if( n === 5 ) {
+        //         //     observer.error("Wrong!");
+        //         // }
+        //         observer.next(n);
+        //     }
+
+        //     observer.complete();
+        // }); 
+        // --------------------------------------------------------------
+
+        let source = Observable.create(observer => {
+
+            let index = 0;
+            let produceValue = () => {
+                observer.next(numbers[index++]);
+
+                if(index < numbers.length) {
+                    setTimeout(produceValue, 2000);
+                } else {
+                    observer.complete();
+                }
+            };
+
+            produceValue();
+        }).map(n => n * 3)
+          .filter(n => n > 4);
+
+
+        // 2 способ подписи на событие------------
+        source.subscribe(
+            value => console.log(`value: ${value}`),
+            (e) => console.log(`error: ${e}`),
+            () => console.log("comlete")
+        );
+        // ----------------------
+
+        // // 1 способ подписи на событие------------
+        // class MyObserver implements Observer<number> {
+        //     next(value) {
+        //         console.log(`value: ${value}`);
+        //     }
+            
+        //     error(e) {
+        //         console.log(`error: ${e}`);
+        //     }
+
+        //     complete() {
+        //         console.log("comlete");
+                
+        //     }
+        // }
+        
+        // source.subscribe(new MyObserver());
+
+        // //--------------------------
+
+    
+    }
+
+}
