3dcb80c7716c3f2fb500d45fabaf77f7813980a4
Gruwa <alex.zolotarenko@gmail.com>
2017-10-09 22:44:46 +0300

    hw_rxjs_20171009

--- practice/hw_rxjs/src/app/app.component.ts
+++ practice/hw_rxjs/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
 
-import { Observable } from "rxjs/Observable";
+// import { Observable } from "rxjs/Observable";
 // import 'rxjs/add/operator/map'; если потребует установить функции
 
 import '../assets/style/style.scss';
@@ -17,70 +18,98 @@ export class AppComponent implements OnInit{
   
   
     ngOnInit() {
-        let numbers = [1,5,10];
-         // let source = Observable.from(numbers); // 1 способ задание потока
 
-        // 2 способ задания потока-------------------
-        // let source = Observable.create(observer => {
-            
-        //     for(let n of numbers) {
-
-        //         // if( n === 5 ) {
-        //         //     observer.error("Wrong!");
-        //         // }
-        //         observer.next(n);
-        //     }
-
-        //     observer.complete();
-        // }); 
-        // --------------------------------------------------------------
-
-        let source = Observable.create(observer => {
-
-            let index = 0;
-            let produceValue = () => {
-                observer.next(numbers[index++]);
-
-                if(index < numbers.length) {
-                    setTimeout(produceValue, 2000);
-                } else {
-                    observer.complete();
-                }
-            };
-
-            produceValue();
-        }).map(n => n * 3)
-          .filter(n => n > 4);
-
-
-        // 2 способ подписи на событие------------
+        let circle = document.getElementById("circle");
+        let source = Observable.fromEvent(document, "mousemove")
+                               .map((e : MouseEvent) => {
+                                   return {
+                                       x: e.clientX,
+                                       y: e.clientY
+                                   };
+                               })                           
+                               .filter(value => value.x < 500)
+                               .delay(300);
+    
+        function onNext(value) {
+            circle.style.left = '70';
+            console.log(circle.style)
+            circle.style.top = '120';
+            console.log(circle.style.top = value.y)
+        }
         source.subscribe(
-            value => console.log(`value: ${value}`),
-            (e) => console.log(`error: ${e}`),
-            () => console.log("comlete")
+            onNext,
+            e => console.log(`error: ${e}`),
+            () => console.log("complete")
         );
-        // ----------------------
 
-        // // 1 способ подписи на событие------------
-        // class MyObserver implements Observer<number> {
-        //     next(value) {
-        //         console.log(`value: ${value}`);
-        //     }
+
+
+
+// //-----------------------------------------------------------------------------        
+//         let numbers = [1,5,10];
+//          // let source = Observable.from(numbers); // 1 способ задание потока
+
+//         // 2 способ задания потока-------------------
+//         // let source = Observable.create(observer => {
             
-        //     error(e) {
-        //         console.log(`error: ${e}`);
-        //     }
+//         //     for(let n of numbers) {
 
-        //     complete() {
-        //         console.log("comlete");
+//         //         // if( n === 5 ) {
+//         //         //     observer.error("Wrong!");
+//         //         // }
+//         //         observer.next(n);
+//         //     }
+
+//         //     observer.complete();
+//         // }); 
+//         // --------------------------------------------------------------
+
+//         let source = Observable.create(observer => {
+
+//             let index = 0;
+//             let produceValue = () => {
+//                 observer.next(numbers[index++]);
+
+//                 if(index < numbers.length) {
+//                     setTimeout(produceValue, 2000);
+//                 } else {
+//                     observer.complete();
+//                 }
+//             };
+
+//             produceValue();
+//         }).map(n => n * 3)
+//           .filter(n => n > 4);
+
+
+//         // 2 способ подписи на событие------------
+//         source.subscribe(
+//             value => console.log(`value: ${value}`),
+//             (e) => console.log(`error: ${e}`),
+//             () => console.log("comlete")
+//         );
+//         // ----------------------
+
+//         // // 1 способ подписи на событие------------
+//         // class MyObserver implements Observer<number> {
+//         //     next(value) {
+//         //         console.log(`value: ${value}`);
+//         //     }
+            
+//         //     error(e) {
+//         //         console.log(`error: ${e}`);
+//         //     }
+
+//         //     complete() {
+//         //         console.log("comlete");
                 
-        //     }
-        // }
+//         //     }
+//         // }
         
-        // source.subscribe(new MyObserver());
+//         // source.subscribe(new MyObserver());
 
-        // //--------------------------
-
+//         // //--------------------------
+// //------------------------------------------------------------------------------
     
     }
 
