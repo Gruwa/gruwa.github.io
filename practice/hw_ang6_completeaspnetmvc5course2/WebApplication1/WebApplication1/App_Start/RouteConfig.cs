ae78ae44dc454782a056b288b4c53d4d0ab7a14b
Aleksey Zolotarenko <alex.zolotarenko@gmail.com>
2018-06-28 18:18:35 +0300

    mvc

--- practice/hw_ang6_completeaspnetmvc5course2/WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ practice/hw_ang6_completeaspnetmvc5course2/WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -10,6 +10,11 @@ namespace WebApplication1
             routes.IgnoreRoute(url: "{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new {controller = "Movies", action = "ByReleaseDate"});
+            
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
--- practice/hw_ang6_completeaspnetmvc5course2/WebApplication1/WebApplication1/Controllers/MoviesController.cs
+++ practice/hw_ang6_completeaspnetmvc5course2/WebApplication1/WebApplication1/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -6,14 +8,39 @@ namespace WebApplication1.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Index()
+        public ActionResult Random()
         {
             var movie = new Movie()
             {
                 Name = "Shrek!"
             };
-            return
-            View(movie);
+//            return View(movie);
+//            return Content("Hello world"); // вернет текст "Hello world"
+//            return HttpNotFound(); // получим ошибку 404
+//            return new EmptyResult(); // получисм на выходе пустую страницу
+            return RedirectToAction(actionName: "Index", controllerName: "Home", routeValues: new { page = 1, sortBy = "name"}); // редирект на другую страницу со страницей 1 и сортировкой по имени
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content(content: "id = " + id); // id  передавать можно как через Movies/edit/1 так и через параметр Movies/edit?id=1 обработается одинаково 
+            // id имя айдишки принятое по умолчанию если будет назван по другому так не пройдет, например NameID
+        }
+// movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(value: sortBy))
+                sortBy = "Name";
+
+            return Content(content: String.Format(format: "pageIndex={0}&sortBy={1}", arg0: pageIndex, arg1: sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(content: year + "/" + month);
         }
     }
 }
