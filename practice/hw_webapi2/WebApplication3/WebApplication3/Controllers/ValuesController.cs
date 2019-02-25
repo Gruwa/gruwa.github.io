a0c405738874f0dee9072a27ce21dfed5c12ee23
Aleksey Zolotarenko <alex.zolotarenko@gmail.com>
2019-02-25 15:07:17 +0200

    start

---
+++ practice/hw_webapi2/WebApplication3/WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
