6a24262192d093ebac32d303428a5137dcc90093
Aleksey Zolotarenko <alex.zolotarenko@gmail.com>
2019-12-21 12:14:00 -0500

    new project

---
+++ practice/hw_dotNetCoreFundamental_OdeToFood/OdeToFood3/OdeToFood3/Pages/Restaurants/_23.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood3.Pages.Restaurants
+{
+    public class _23Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
